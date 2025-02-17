6adf63faff1599d1a39f3bfe8866c357295b0119
WJ <1220253798@qq.com>
2025-02-17 09:03:34 +0800

    refactor: 添加minPixelValue选项到postcss配置文件

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -8,6 +8,7 @@ export default {
     //   rootValue: 37.5, // Vant 官方根字体大小是 37.5
     //   propList: ['*'],
     //   selectorBlackList: ['.norem', /.*px$/], // 过滤掉.norem-开头/px结尾 的class，不进行rem转换
+    //   minPixelValue: 2 // 最小转换的像素值，小于该值的 px 不转换
     // }),
   ],
 }
