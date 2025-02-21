f894467139f21e77da192f2859ec680589a54920
WJ <1220253798@qq.com>
2025-02-21 13:51:28 +0800

    refactor: Add auth directive for authentication functionality

--- src/directives/index.ts
+++ src/directives/index.ts
@@ -1,11 +1,12 @@
 import type { Directive, Plugin } from 'vue'
+import auth from './auth'
 import loading from './loading'
 
 export type CustomDirective = Directive & {
   name: string
 }
 
-const directives: CustomDirective[] = [loading]
+const directives: CustomDirective[] = [loading, auth]
 
 const directivePlugins: Plugin = {
   install(app) {
