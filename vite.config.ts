59a7190de64ce655107b482106da6950c93f30b6
WJ <1220253798@qq.com>
2025-02-24 16:54:12 +0800

    refactor: Remove unused envDir configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,6 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, './env') as ImportMetaEnv
 
   return {
-    envDir: './env',
     resolve: {
       alias: {
         '@/': `${path.resolve(__dirname, 'src')}/`,
