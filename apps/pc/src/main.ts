da4eef4225d31c3d53f5e52260b23b7b924e8db9
WJ <1220253798@qq.com>
2025-02-28 14:52:11 +0800

    chore: Update npm dependencies for h5 and pc packages

--- apps/pc/src/main.ts
+++ apps/pc/src/main.ts
@@ -1,13 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import directivePlugins from './directives'
 
+import directivePlugins from './directives'
 import router from './router'
 import '@unocss/reset/tailwind.css'
 import 'uno.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
-import './styles/index.css'
 
+import './styles/index.css'
 import './router/permission'
 
 const app = createApp(App)
